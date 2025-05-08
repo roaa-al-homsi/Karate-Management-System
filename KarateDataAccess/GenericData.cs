b49828034724ae7621158e415b6b4fd66631025e
roaa-al-homsi <roaakokalhomsi@gmail.com>
2025-05-09 01:57:25 +0300

    ADD methods to SettingData class

--- KarateDataAccess/GenericData.cs
+++ KarateDataAccess/GenericData.cs
@@ -1,6 +1,119 @@
-﻿namespace KarateDataAccess
+﻿using System;
+using System.Configuration;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+
+namespace KarateDataAccess
 {
     public static class GenericData
     {
+        static public DataTable All(string query)
+        {
+            DataTable dt = new DataTable();
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(SettingData.ConnectionString))
+                {
+                    connection.Open();
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+
+
+                        using (SqlDataReader Reader = command.ExecuteReader())
+                        {
+                            if (Reader.HasRows)
+                            {
+                                dt.Load(Reader);
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex) { LogException(ex.Message, EventLogEntryType.Error); }
+            return dt;
+        }
+        static public bool Delete<T>(string query, string ParameterName, T DeleteBy)
+        {
+            int RowsAffected = 0;
+            using (SqlConnection connection = new SqlConnection(SettingData.ConnectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue(ParameterName, DeleteBy);
+
+                    try
+                    {
+                        connection.Open();
+                        RowsAffected = command.ExecuteNonQuery();
+                    }
+                    catch (Exception ex)
+                    {
+                        LogException(ex.Message, EventLogEntryType.Error);
+                        return false;
+                    }
+                }
+            }
+
+            return RowsAffected > 0;
+        }
+        static public bool Exist<T>(string query, string ParameterName, T ParameterValue)
+        {
+            bool IsFound = false;
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(SettingData.ConnectionString))
+                {
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue(ParameterName, ParameterValue);
+
+                        connection.Open();
+                        using (SqlDataReader Reader = command.ExecuteReader())
+                        {
+                            IsFound = Reader.HasRows;
+                        }
+                    }
+                }
+
+            }
+            catch (Exception ex) { LogException(ex.Message, EventLogEntryType.Error); }
+            return IsFound;
+
+        }
+        static public void LogException(string EventMessage, EventLogEntryType eventLogEntryType)
+        {
+            string sourceName = ConfigurationManager.AppSettings["SourceAppName"];
+            try
+            {
+                if (!EventLog.SourceExists(sourceName))
+                {
+                    EventLog.CreateEventSource(sourceName, ConfigurationManager.AppSettings["EventLogName"]);
+
+                }
+                EventLog.WriteEntry(sourceName, EventMessage, eventLogEntryType);
+            }
+            catch (Exception ex)
+            {
+                EventLog.WriteEntry(sourceName, $"Exception in logException method is : {ex.Message}", EventLogEntryType.Error);
+
+            }
+        }
+
     }
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
