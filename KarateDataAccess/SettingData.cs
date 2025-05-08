75c290b2d641968e6d970352943aa7e37bf6c576
roaa-al-homsi <roaakokalhomsi@gmail.com>
2025-05-09 01:52:14 +0300

    ADD SettingData class to Access Layer

---
+++ KarateDataAccess/SettingData.cs
@@ -0,0 +1,10 @@
+﻿using System.Configuration;
+
+namespace KarateDataAccess
+{
+    public static class SettingData
+    {
+        public static string ConnectionString = ConfigurationManager.ConnectionStrings["ConnectionString"].ConnectionString;
+
+    }
+}
