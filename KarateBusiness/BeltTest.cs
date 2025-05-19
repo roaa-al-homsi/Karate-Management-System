bf4645b0b746e42695135fd249a6c24862ab1451
roaa-al-homsi <roaakokalhomsi@gmail.com>
2025-05-20 00:50:09 +0300

    ADD BeltTest classes to Business and Access layers

---
+++ KarateBusiness/BeltTest.cs
@@ -0,0 +1,106 @@
+﻿using KarateDataAccess;
+using System;
+using System.Data;
+namespace KarateBusiness
+{
+    public class BeltTest
+    {
+        private enum Mode { Add, Update }
+        private Mode _mode;
+        public int id { get; set; }
+        public int rankId { get; set; }
+        public int memberId { get; set; }
+        public bool result { get; set; }
+        public DateTime date { get; set; }
+        public int testedByInstructorID { get; set; }
+        public int paymentId { get; set; }
+
+        public BeltTest()
+        {
+            this.id = 0;
+            this.rankId = -1;
+            this.memberId = -1;
+            this.result = false;
+            this.date = DateTime.MinValue;
+            this.testedByInstructorID = 0;
+            this.paymentId = -1;
+
+            _mode = Mode.Add;
+        }
+        private BeltTest(int id, int rankId, int memberId, bool result, DateTime date, int testedByInstructorID, int paymentId)
+        {
+            this.id = id;
+            this.rankId = rankId;
+            this.memberId = memberId;
+            this.result = result;
+            this.date = date;
+            this.testedByInstructorID = testedByInstructorID;
+            this.paymentId = paymentId;
+
+
+            _mode = Mode.Update;
+        }
+        private bool _Add()
+        {
+            this.id =
+                        BeltTestData.Add(this.rankId, this.memberId, this.result, this.date, this.testedByInstructorID, this.paymentId);
+            return (this.id != -1);
+        }
+
+        private bool _Update()
+        {
+            return BeltTestData.Update(this.id, this.rankId, this.memberId, this.result, this.date, this.testedByInstructorID, this.paymentId);
+        }
+        public bool Save()
+        {
+
+            switch (_mode)
+            {
+                case Mode.Add:
+                    {
+                        if (_Add())
+                        {
+                            _mode = Mode.Update;
+                            return true;
+                        }
+                        return false;
+                    }
+                case Mode.Update: return _Update();
+            }
+            return false;
+        }
+        public static bool Exist(int id)
+        {
+            return BeltTestData.Exist(id);
+        }
+        public static bool Delete(int id)
+        {
+            if (!Exist(id))
+            {
+                return false;
+            }
+            else { return BeltTestData.Delete(id); }
+        }
+        public static DataTable All()
+        {
+            return BeltTestData.All();
+        }
+        public static BeltTest Find(int id)
+        {
+            int rankId = -1;
+            int memberId = -1;
+            bool result = false;
+            DateTime date = DateTime.MinValue;
+            int testedByInstructorID = -1;
+            int paymentId = -1;
+
+            if (BeltTestData.Get(id, ref rankId, ref memberId, ref result, ref date, ref testedByInstructorID, ref paymentId))
+            {
+                return new BeltTest(id, rankId, memberId, result, date, testedByInstructorID, paymentId);
+            }
+            return null;
+        }
+    }
+
+
+}
---
+++ KarateDataAccess/BeltTestData.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+
+namespace KarateDataAccess
+{
+    public static class BeltTestData
+    {
+        public static int Add(int rankId, int memberId, bool result, DateTime date, int testedByInstructorID, int paymentId)
+        {
+            int newId = 0;
+            string query = "insert into BeltTests (rankId,memberId,result,date,testedByInstructorID,paymentId) values (@rankId,@memberId,@result,@date,@testedByInstructorID,@paymentId) SELECT SCOPE_IDENTITY(); ";
+            using (SqlConnection connection = new SqlConnection(SettingData.ConnectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+
+                    command.Parameters.AddWithValue("@rankId", rankId);
+                    command.Parameters.AddWithValue("@memberId", memberId);
+                    command.Parameters.AddWithValue("@result", result);
+                    command.Parameters.AddWithValue("@date", date);
+                    command.Parameters.AddWithValue("@testedByInstructorID", testedByInstructorID);
+                    command.Parameters.AddWithValue("@paymentId", paymentId);
+
+                    try
+                    {
+                        connection.Open();
+                        object result = command.ExecuteScalar();
+                        if (result != null && int.TryParse(result.ToString(), out int insertedID))
+                        {
+                            newId = insertedID;
+                        }
+
+                    }
+                    catch (Exception ex) { GenericData.LogException(ex.Message, EventLogEntryType.Error); }
+                }
+            }
+
+            return newId;
+        }
+        public static bool Update(int id, int rankId, int memberId, bool result, DateTime date, int testedByInstructorID, int paymentId)
+        {
+            int RowsAffected = 0;
+            string query = "update BeltTests set rankId = @rankId,memberId = @memberId,result = @result,date = @date,testedByInstructorID = @testedByInstructorID,paymentId = @paymentId  WHERE id=@id;";
+            using (SqlConnection connection = new SqlConnection(SettingData.ConnectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@id", id);
+                    command.Parameters.AddWithValue("@rankId", rankId);
+                    command.Parameters.AddWithValue("@memberId", memberId);
+                    command.Parameters.AddWithValue("@result", result);
+                    command.Parameters.AddWithValue("@date", date);
+                    command.Parameters.AddWithValue("@testedByInstructorID", testedByInstructorID);
+                    command.Parameters.AddWithValue("@paymentId", paymentId);
+
+                    try
+                    {
+                        connection.Open();
+                        RowsAffected = command.ExecuteNonQuery();
+                    }
+                    catch (Exception ex) { GenericData.LogException(ex.Message, EventLogEntryType.Error); }
+                }
+            }
+
+            return RowsAffected > 0;
+        }
+        public static bool Get(int id, ref int rankId, ref int memberId, ref bool result, ref DateTime date, ref int testedByInstructorID, ref int paymentId)
+        {
+            bool IsFound = false;
+            string query = "select * from BeltTests  WHERE id=@id;";
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(SettingData.ConnectionString))
+                {
+                    connection.Open();
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@id", id);
+
+
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                IsFound = true;
+                                id = (int)reader["id"];
+                                rankId = (int)reader["rankId"];
+                                memberId = (int)reader["memberId"];
+                                result = (bool)reader["result"];
+                                date = (DateTime)reader["date"];
+                                testedByInstructorID = (int)reader["testedByInstructorID"];
+                                paymentId = (int)reader["paymentId"];
+
+
+                            }
+                            else
+                            {
+                                IsFound = false;
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex) { GenericData.LogException(ex.Message, EventLogEntryType.Error); }
+            return IsFound;
+        }
+
+        static public DataTable All()
+        {
+            return GenericData.All("select * from BeltTests");
+        }
+        static public bool Delete(int id)
+        {
+            return GenericData.Delete("delete BeltTests where id = @id", "@id", id);
+        }
+        static public bool Exist(int id)
+        {
+            return GenericData.Exist("select Found=1 from BeltTests where id= @id", "@id", id);
+        }
+
+    }
+
+
+}
+
