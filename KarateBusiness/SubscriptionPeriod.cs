97ae0fb643dacf862e9a88234327a7858cb09bcf
roaa-al-homsi <roaakokalhomsi@gmail.com>
2025-05-20 00:34:13 +0300

    ADD SubscriptionPeriod classes to Business and Access layers

---
+++ KarateBusiness/SubscriptionPeriod.cs
@@ -0,0 +1,102 @@
+﻿using KarateDataAccess;
+using System;
+using System.Data;
+
+namespace KarateBusiness
+{
+    public class SubscriptionPeriod
+    {
+        private enum Mode { Add, Update }
+        private Mode _mode;
+        public int id { get; set; }
+        public DateTime startDate { get; set; }
+        public DateTime endDate { get; set; }
+        public decimal fees { get; set; }
+        public int memberId { get; set; }
+        public int paymentId { get; set; }
+
+        public SubscriptionPeriod()
+        {
+            this.id = 0;
+            this.startDate = DateTime.MinValue;
+            this.endDate = DateTime.MinValue;
+            this.fees = 0;
+            this.memberId = -1;
+            this.paymentId = -1;
+
+            _mode = Mode.Add;
+        }
+        private SubscriptionPeriod(int id, DateTime startDate, DateTime endDate, decimal fees, int memberId, int paymentId)
+        {
+            this.id = id;
+            this.startDate = startDate;
+            this.endDate = endDate;
+            this.fees = fees;
+            this.memberId = memberId;
+            this.paymentId = paymentId;
+
+
+            _mode = Mode.Update;
+        }
+        private bool _Add()
+        {
+            this.id = SubscriptionPeriodData.Add(this.startDate, this.endDate, this.fees, this.memberId, this.paymentId);
+            return (this.id != -1);
+        }
+
+        private bool _Update()
+        {
+            return SubscriptionPeriodData.Update(this.id, this.startDate, this.endDate, this.fees, this.memberId, this.paymentId);
+        }
+        public bool Save()
+        {
+
+            switch (_mode)
+            {
+                case Mode.Add:
+                    {
+                        if (_Add())
+                        {
+                            _mode = Mode.Update;
+                            return true;
+                        }
+                        return false;
+                    }
+                case Mode.Update: return _Update();
+            }
+            return false;
+        }
+        public static bool Exist(int id)
+        {
+            return SubscriptionPeriodData.Exist(id);
+        }
+        public static bool Delete(int id)
+        {
+            if (!Exist(id))
+            {
+                return false;
+            }
+            else { return SubscriptionPeriodData.Delete(id); }
+        }
+        public static DataTable All()
+        {
+            return SubscriptionPeriodData.All();
+        }
+        public static SubscriptionPeriod Find(int id)
+        {
+            DateTime startDate = DateTime.MinValue;
+            DateTime endDate = DateTime.MinValue;
+            decimal fees = 0;
+            int memberId = -1;
+            int paymentId = -1;
+
+            if (SubscriptionPeriodData.Get(id, ref startDate, ref endDate, ref fees, ref memberId, ref paymentId))
+            {
+                return new SubscriptionPeriod(id, startDate, endDate, fees, memberId, paymentId);
+            }
+            return null;
+        }
+    }
+
+
+}
---
+++ KarateDataAccess/SubscriptionPeriodData.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+
+namespace KarateDataAccess
+{
+    public static class SubscriptionPeriodData
+    {
+        public static int Add(DateTime startDate, DateTime endDate, decimal fees, int memberId, int paymentId)
+        {
+            int newId = 0;
+            string query = "insert into SubscriptionPeriods (startDate,endDate,fees,memberId,paymentId) values (@startDate,@endDate,@fees,@memberId,@paymentId) SELECT SCOPE_IDENTITY(); ";
+            using (SqlConnection connection = new SqlConnection(SettingData.ConnectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+
+                    command.Parameters.AddWithValue("@startDate", startDate);
+                    command.Parameters.AddWithValue("@endDate", endDate);
+                    command.Parameters.AddWithValue("@fees", fees);
+                    command.Parameters.AddWithValue("@memberId", memberId);
+                    command.Parameters.AddWithValue("@paymentId", paymentId);
+
+                    try
+                    {
+                        connection.Open();
+                        object result = command.ExecuteScalar();
+                        if (result != null && int.TryParse(result.ToString(), out int insertedID))
+                        {
+                            newId = insertedID;
+                        }
+
+                    }
+                    catch (Exception ex) { GenericData.LogException(ex.Message, EventLogEntryType.Error); }
+                }
+            }
+
+            return newId;
+        }
+        public static bool Update(int id, DateTime startDate, DateTime endDate, decimal fees, int memberId, int paymentId)
+        {
+            int RowsAffected = 0;
+            string query = "update SubscriptionPeriods set id = @id,startDate = @startDate,endDate = @endDate,fees = @fees,memberId = @memberId,paymentId = @paymentId  WHERE id=@id;";
+            using (SqlConnection connection = new SqlConnection(SettingData.ConnectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@id", id);
+                    command.Parameters.AddWithValue("@startDate", startDate);
+                    command.Parameters.AddWithValue("@endDate", endDate);
+                    command.Parameters.AddWithValue("@fees", fees);
+                    command.Parameters.AddWithValue("@memberId", memberId);
+                    command.Parameters.AddWithValue("@paymentId", paymentId);
+
+                    try
+                    {
+                        connection.Open();
+                        RowsAffected = command.ExecuteNonQuery();
+                    }
+                    catch (Exception ex) { GenericData.LogException(ex.Message, EventLogEntryType.Error); }
+                }
+            }
+
+            return RowsAffected > 0;
+        }
+        public static bool Get(int id, ref DateTime startDate, ref DateTime endDate, ref decimal fees, ref int memberId, ref int paymentId)
+        {
+            bool IsFound = false;
+            string query = "select * from SubscriptionPeriods  WHERE id=@id;";
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(SettingData.ConnectionString))
+                {
+                    connection.Open();
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@id", id);
+
+
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                IsFound = true;
+                                id = (int)reader["id"];
+                                startDate = (DateTime)reader["startDate"];
+                                endDate = (DateTime)reader["endDate"];
+                                fees = (decimal)reader["fees"];
+                                memberId = (int)reader["memberId"];
+                                paymentId = (int)reader["paymentId"];
+
+
+                            }
+                            else
+                            {
+                                IsFound = false;
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex) { GenericData.LogException(ex.Message, EventLogEntryType.Error); }
+            return IsFound;
+        }
+
+        static public DataTable All()
+        {
+            return GenericData.All("select * from SubscriptionPeriods");
+        }
+        static public bool Delete(int id)
+        {
+            return GenericData.Delete("delete SubscriptionPeriods where id = @id", "@id", id);
+        }
+        static public bool Exist(int id)
+        {
+            return GenericData.Exist("select Found=1 from SubscriptionPeriods where id= @id", "@id", id);
+        }
+
+
+    }
+
+}
+
