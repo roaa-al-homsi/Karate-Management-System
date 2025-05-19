211021e98b325906c2d486e7b12c9ef8cfdfba64
roaa-al-homsi <roaakokalhomsi@gmail.com>
2025-05-20 00:17:53 +0300

    ADD MemberInstructor classes to Business and Access layers

---
+++ KarateBusiness/MemberInstructors.cs
@@ -0,0 +1,95 @@
+﻿using KarateDataAccess;
+using System;
+using System.Data;
+
+namespace KarateBusiness
+{
+    public class MemberInstructor
+    {
+        private enum Mode { Add, Update }
+        private Mode _mode;
+        public int id { get; set; }
+        public int memberId { get; set; }
+        public int instructorId { get; set; }
+        public DateTime assignDate { get; set; }
+
+        public MemberInstructor()
+        {
+            this.id = 0;
+            this.memberId = -1;
+            this.instructorId = -1;
+            this.assignDate = DateTime.MinValue;
+
+            _mode = Mode.Add;
+        }
+        private MemberInstructor(int id, int memberId, int instructorId, DateTime assignDate)
+        {
+            this.id = id;
+            this.memberId = memberId;
+            this.instructorId = instructorId;
+            this.assignDate = assignDate;
+
+
+            _mode = Mode.Update;
+        }
+        private bool _Add()
+        {
+            this.id = MemberInstructorsData.Add(this.memberId, this.instructorId, this.assignDate);
+
+            return (this.id != -1);
+        }
+
+        private bool _Update()
+        {
+            return MemberInstructorsData.Update(this.id, this.memberId, this.instructorId, this.assignDate);
+        }
+        public bool Save()
+        {
+
+            switch (_mode)
+            {
+                case Mode.Add:
+                    {
+                        if (_Add())
+                        {
+                            _mode = Mode.Update;
+                            return true;
+                        }
+                        return false;
+                    }
+                case Mode.Update: return _Update();
+            }
+            return false;
+        }
+        public static bool Exist(int id)
+        {
+            return MemberInstructorsData.Exist(id);
+        }
+        public static bool Delete(int id)
+        {
+            if (!Exist(id))
+            {
+                return false;
+            }
+            else { return MemberInstructorsData.Delete(id); }
+        }
+        public static DataTable All()
+        {
+            return MemberInstructorsData.All();
+        }
+        public static MemberInstructor Find(int id)
+        {
+            int memberId = -1;
+            int instructorId = -1;
+            DateTime assignDate = DateTime.MinValue;
+
+            if (MemberInstructorsData.Get(id, ref memberId, ref instructorId, ref assignDate))
+            {
+                return new MemberInstructor(id, memberId, instructorId, assignDate);
+            }
+            return null;
+        }
+    }
+
+
+}
---
+++ KarateDataAccess/MemberInstructorsData.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+
+namespace KarateDataAccess
+{
+    public static class MemberInstructorsData
+    {
+        public static int Add(int memberId, int instructorId, DateTime assignDate)
+        {
+            int newId = 0;
+            string query = "insert into MemberInstructors (memberId,instructorId,assignDate) values (@memberId,@instructorId,@assignDate) SELECT SCOPE_IDENTITY(); ";
+            using (SqlConnection connection = new SqlConnection(SettingData.ConnectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+
+                    command.Parameters.AddWithValue("@memberId", memberId);
+                    command.Parameters.AddWithValue("@instructorId", instructorId);
+                    command.Parameters.AddWithValue("@assignDate", assignDate);
+
+                    try
+                    {
+                        connection.Open();
+                        object result = command.ExecuteScalar();
+                        if (result != null && int.TryParse(result.ToString(), out int insertedID))
+                        {
+                            newId = insertedID;
+                        }
+
+                    }
+                    catch (Exception ex) { GenericData.LogException(ex.Message, EventLogEntryType.Error); }
+                }
+            }
+
+            return newId;
+        }
+        public static bool Update(int id, int memberId, int instructorId, DateTime assignDate)
+        {
+            int RowsAffected = 0;
+            string query = "update MemberInstructors memberId = @memberId,instructorId = @instructorId,assignDate = @assignDate  WHERE id=@id;";
+            using (SqlConnection connection = new SqlConnection(SettingData.ConnectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@id", id);
+                    command.Parameters.AddWithValue("@memberId", memberId);
+                    command.Parameters.AddWithValue("@instructorId", instructorId);
+                    command.Parameters.AddWithValue("@assignDate", assignDate);
+
+                    try
+                    {
+                        connection.Open();
+                        RowsAffected = command.ExecuteNonQuery();
+                    }
+                    catch (Exception ex) { GenericData.LogException(ex.Message, EventLogEntryType.Error); }
+                }
+            }
+
+            return RowsAffected > 0;
+        }
+        public static bool Get(int id, ref int memberId, ref int instructorId, ref DateTime assignDate)
+        {
+            bool IsFound = false;
+            string query = "select * from MemberInstructors  WHERE id=@id;";
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(SettingData.ConnectionString))
+                {
+                    connection.Open();
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@id", id);
+
+
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                IsFound = true;
+                                id = (int)reader["id"];
+                                memberId = (int)reader["memberId"];
+                                instructorId = (int)reader["instructorId"];
+                                assignDate = (DateTime)reader["assignDate"];
+
+
+                            }
+                            else
+                            {
+                                IsFound = false;
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex) { GenericData.LogException(ex.Message, EventLogEntryType.Error); }
+            return IsFound;
+        }
+
+        static public DataTable All()
+        {
+            return GenericData.All("select * from MemberInstructors");
+        }
+        static public bool Delete(int id)
+        {
+            return GenericData.Delete("delete MemberInstructors where id = @id", "@id", id);
+        }
+        static public bool Exist(int id)
+        {
+            return GenericData.Exist("select Found=1 from MemberInstructors where id= @id", "@id", id);
+        }
+
+    }
+
+
+}
+
