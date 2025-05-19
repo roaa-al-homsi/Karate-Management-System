2096cfd63494f58882c18410586de0fbc1bedc36
roaa-al-homsi <roaakokalhomsi@gmail.com>
2025-05-20 00:24:49 +0300

    ADD Payments classes to Business and Access layers

---
+++ KarateBusiness/Payment.cs
@@ -0,0 +1,95 @@
+﻿using KarateDataAccess;
+using System;
+using System.Data;
+
+namespace KarateBusiness
+{
+    public class Payment
+    {
+        private enum Mode { Add, Update }
+        private Mode _mode;
+        public int id { get; set; }
+        public decimal amount { get; set; }
+        public DateTime date { get; set; }
+        public int memberId { get; set; }
+
+        public Payment()
+        {
+            this.id = 0;
+            this.amount = 0;
+            this.date = DateTime.MinValue;
+            this.memberId = -1;
+
+            _mode = Mode.Add;
+        }
+        private Payment(int id, decimal amount, DateTime date, int memberId)
+        {
+            this.id = id;
+            this.amount = amount;
+            this.date = date;
+            this.memberId = memberId;
+
+
+            _mode = Mode.Update;
+        }
+        private bool _Add()
+        {
+            this.id = PaymentData.Add(this.amount, this.date, this.memberId);
+
+            return (this.id != -1);
+        }
+
+        private bool _Update()
+        {
+            return PaymentData.Update(this.id, this.amount, this.date, this.memberId);
+        }
+        public bool Save()
+        {
+
+            switch (_mode)
+            {
+                case Mode.Add:
+                    {
+                        if (_Add())
+                        {
+                            _mode = Mode.Update;
+                            return true;
+                        }
+                        return false;
+                    }
+                case Mode.Update: return _Update();
+            }
+            return false;
+        }
+        public static bool Exist(int id)
+        {
+            return PaymentData.Exist(id);
+        }
+        public static bool Delete(int id)
+        {
+            if (!Exist(id))
+            {
+                return false;
+            }
+            else { return PaymentData.Delete(id); }
+        }
+        public static DataTable All()
+        {
+            return PaymentData.All();
+        }
+        public static Payment Find(int id)
+        {
+            decimal amount = 0;
+            DateTime date = DateTime.MinValue;
+            int memberId = -1;
+
+            if (PaymentData.Get(id, ref amount, ref date, ref memberId))
+            {
+                return new Payment(id, amount, date, memberId);
+            }
+            return null;
+        }
+    }
+
+
+}
---
+++ KarateDataAccess/PaymentData.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+
+namespace KarateDataAccess
+{
+    public static class PaymentData
+    {
+        public static int Add(decimal amount, DateTime date, int memberId)
+        {
+            int newId = 0;
+            string query = "insert into Payments (amount,date,memberId) values (@amount,@date,@memberId) SELECT SCOPE_IDENTITY(); ";
+            using (SqlConnection connection = new SqlConnection(SettingData.ConnectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+
+                    command.Parameters.AddWithValue("@amount", amount);
+                    command.Parameters.AddWithValue("@date", date);
+                    command.Parameters.AddWithValue("@memberId", memberId);
+
+                    try
+                    {
+                        connection.Open();
+                        object result = command.ExecuteScalar();
+                        if (result != null && int.TryParse(result.ToString(), out int insertedID))
+                        {
+                            newId = insertedID;
+                        }
+
+                    }
+                    catch (Exception ex) { GenericData.LogException(ex.Message, EventLogEntryType.Error); }
+                }
+            }
+
+            return newId;
+        }
+        public static bool Update(int id, decimal amount, DateTime date, int memberId)
+        {
+            int RowsAffected = 0;
+            string query = "update Payments set id = @id,amount = @amount,date = @date,memberId = @memberId  WHERE id=@id;";
+            using (SqlConnection connection = new SqlConnection(SettingData.ConnectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@id", id);
+                    command.Parameters.AddWithValue("@amount", amount);
+                    command.Parameters.AddWithValue("@date", date);
+                    command.Parameters.AddWithValue("@memberId", memberId);
+
+                    try
+                    {
+                        connection.Open();
+                        RowsAffected = command.ExecuteNonQuery();
+                    }
+                    catch (Exception ex) { GenericData.LogException(ex.Message, EventLogEntryType.Error); }
+                }
+            }
+
+            return RowsAffected > 0;
+        }
+        public static bool Get(int id, ref decimal amount, ref DateTime date, ref int memberId)
+        {
+            bool IsFound = false;
+            string query = "select * from Payments  WHERE id=@id;";
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(SettingData.ConnectionString))
+                {
+                    connection.Open();
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@id", id);
+
+
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                IsFound = true;
+                                id = (int)reader["id"];
+                                amount = (decimal)reader["amount"];
+                                date = (DateTime)reader["date"];
+                                memberId = (int)reader["memberId"];
+
+
+                            }
+                            else
+                            {
+                                IsFound = false;
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex) { GenericData.LogException(ex.Message, EventLogEntryType.Error); }
+            return IsFound;
+        }
+
+        static public DataTable All()
+        {
+            return GenericData.All("select * from Payments");
+        }
+        static public bool Delete(int id)
+        {
+            return GenericData.Delete("delete Payments where id = @id", "@id", id);
+        }
+        static public bool Exist(int id)
+        {
+            return GenericData.Exist("select Found=1 from Payments where id= @id", "@id", id);
+        }
+
+    }
+
+}
