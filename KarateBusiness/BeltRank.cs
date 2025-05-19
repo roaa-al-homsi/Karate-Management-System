8383f4e6c4c6784934a1bac81ec9e74f7517e1b8
roaa-al-homsi <roaakokalhomsi@gmail.com>
2025-05-20 00:44:58 +0300

    ADD BeltRank classes to Business and Access layers

---
+++ KarateBusiness/BeltRank.cs
@@ -0,0 +1,91 @@
+﻿using KarateDataAccess;
+using System.Data;
+
+namespace KarateBusiness
+{
+    public class BeltRank
+    {
+        private enum Mode { Add, Update }
+        private Mode _mode;
+        public int id { get; set; }
+        public string name { get; set; }
+        public decimal testFees { get; set; }
+
+        public BeltRank()
+        {
+            this.id = 0;
+            this.name = string.Empty;
+            this.testFees = 0;
+
+            _mode = Mode.Add;
+        }
+        private BeltRank(int id, string name, decimal testFees)
+        {
+            this.id = id;
+            this.name = name;
+            this.testFees = testFees;
+
+
+            _mode = Mode.Update;
+        }
+        private bool _Add()
+        {
+            this.id =
+                        BeltRankData.Add(this.name, this.testFees);
+            return (this.id != -1);
+        }
+
+        private bool _Update()
+        {
+            return BeltRankData.Update(this.id, this.name, this.testFees);
+        }
+        public bool Save()
+        {
+
+            switch (_mode)
+            {
+                case Mode.Add:
+                    {
+                        if (_Add())
+                        {
+                            _mode = Mode.Update;
+                            return true;
+                        }
+                        return false;
+                    }
+                case Mode.Update: return _Update();
+            }
+            return false;
+        }
+        public static bool Exist(int id)
+        {
+            return BeltRankData.Exist(id);
+        }
+        public static bool Delete(int id)
+        {
+            if (!Exist(id))
+            {
+                return false;
+            }
+            else { return BeltRankData.Delete(id); }
+        }
+        public static DataTable All()
+        {
+            return BeltRankData.All();
+        }
+        public static BeltRank Find(int id)
+        {
+            string name = string.Empty;
+            decimal testFees = 0;
+
+            if (BeltRankData.Get(id, ref name, ref testFees))
+            {
+                return new BeltRank(id, name, testFees);
+            }
+            return null;
+        }
+    }
+
+
+}
+
---
+++ KarateDataAccess/BeltRankData.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+
+
+namespace KarateDataAccess
+{
+    public static class BeltRankData
+    {
+        public static int Add(string name, decimal testFees)
+        {
+            int newId = 0;
+            string query = "insert into BeltRanks (name,testFees) values (@name,@testFees) SELECT SCOPE_IDENTITY(); ";
+            using (SqlConnection connection = new SqlConnection(SettingData.ConnectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+
+                    command.Parameters.AddWithValue("@name", name);
+                    command.Parameters.AddWithValue("@testFees", testFees);
+
+                    try
+                    {
+                        connection.Open();
+                        object result = command.ExecuteScalar();
+                        if (result != null && int.TryParse(result.ToString(), out int insertedID))
+                        {
+                            newId = insertedID;
+                        }
+
+                    }
+                    catch (Exception ex) { GenericData.LogException(ex.Message, EventLogEntryType.Error); }
+                }
+            }
+
+            return newId;
+        }
+        public static bool Update(int id, string name, decimal testFees)
+        {
+            int RowsAffected = 0;
+            string query = "update BeltRanks set name = @name,testFees = @testFees  WHERE id=@id;";
+            using (SqlConnection connection = new SqlConnection(SettingData.ConnectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@id", id);
+                    command.Parameters.AddWithValue("@name", name);
+                    command.Parameters.AddWithValue("@testFees", testFees);
+
+                    try
+                    {
+                        connection.Open();
+                        RowsAffected = command.ExecuteNonQuery();
+                    }
+                    catch (Exception ex) { GenericData.LogException(ex.Message, EventLogEntryType.Error); }
+                }
+            }
+
+            return RowsAffected > 0;
+        }
+        public static bool Get(int id, ref string name, ref decimal testFees)
+        {
+            bool IsFound = false;
+            string query = "select * from BeltRanks  WHERE id=@id;";
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(SettingData.ConnectionString))
+                {
+                    connection.Open();
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@id", id);
+
+
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                IsFound = true;
+                                id = (int)reader["id"];
+                                name = (string)reader["name"];
+                                testFees = (decimal)reader["testFees"];
+
+
+                            }
+                            else
+                            {
+                                IsFound = false;
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex) { GenericData.LogException(ex.Message, EventLogEntryType.Error); }
+            return IsFound;
+        }
+
+        static public DataTable All()
+        {
+            return GenericData.All("select * from BeltRanks");
+        }
+        static public bool Delete(int id)
+        {
+            return GenericData.Delete("delete BeltRanks where id = @id", "@id", id);
+        }
+        static public bool Exist(int id)
+        {
+            return GenericData.Exist("select Found=1 from BeltRanks where id= @id", "@id", id);
+        }
+
+
+    }
+
+
+}
