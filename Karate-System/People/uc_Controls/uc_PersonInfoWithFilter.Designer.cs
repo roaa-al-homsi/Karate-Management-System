0033898116c762a7d091f1790c08b27983a21bff
roaa-al-homsi <roaakokalhomsi@gmail.com>
2025-05-25 10:42:35 +0300

    ADD uc_PersonInfoWithFilter Without coding

---
+++ Karate-System/People/uc_Controls/uc_PersonInfoWithFilter.Designer.cs
@@ -0,0 +1,173 @@
+﻿namespace KarateSystem.People.uc_Controls
+{
+    partial class uc_PersonInfoWithFilter
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(uc_PersonInfoWithFilter));
+            this.gbFilter = new Guna.UI2.WinForms.Guna2GroupBox();
+            this.btnAdd = new Guna.UI2.WinForms.Guna2CirclePictureBox();
+            this.btnFind = new Guna.UI2.WinForms.Guna2CirclePictureBox();
+            this.txtFilterValue = new Guna.UI2.WinForms.Guna2TextBox();
+            this.cmbFilterBy = new Guna.UI2.WinForms.Guna2ComboBox();
+            this.guna2HtmlLabel1 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.uc_PersonInfoCard1 = new KarateSystem.People.uc_Controls.uc_PersonInfoCard();
+            this.gbFilter.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.btnAdd)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnFind)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gbFilter
+            // 
+            this.gbFilter.BorderColor = System.Drawing.Color.Black;
+            this.gbFilter.Controls.Add(this.btnAdd);
+            this.gbFilter.Controls.Add(this.btnFind);
+            this.gbFilter.Controls.Add(this.txtFilterValue);
+            this.gbFilter.Controls.Add(this.cmbFilterBy);
+            this.gbFilter.Controls.Add(this.guna2HtmlLabel1);
+            this.gbFilter.CustomBorderColor = System.Drawing.Color.Gainsboro;
+            this.gbFilter.Font = new System.Drawing.Font("Andalus", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.gbFilter.ForeColor = System.Drawing.Color.Black;
+            this.gbFilter.Location = new System.Drawing.Point(3, 3);
+            this.gbFilter.Name = "gbFilter";
+            this.gbFilter.Size = new System.Drawing.Size(723, 125);
+            this.gbFilter.TabIndex = 1;
+            this.gbFilter.Text = "Filter";
+            // 
+            // btnAdd
+            // 
+            this.btnAdd.BackColor = System.Drawing.Color.Transparent;
+            this.btnAdd.Image = ((System.Drawing.Image)(resources.GetObject("btnAdd.Image")));
+            this.btnAdd.ImageRotate = 0F;
+            this.btnAdd.Location = new System.Drawing.Point(594, 58);
+            this.btnAdd.Name = "btnAdd";
+            this.btnAdd.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            this.btnAdd.Size = new System.Drawing.Size(53, 37);
+            this.btnAdd.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.btnAdd.TabIndex = 5;
+            this.btnAdd.TabStop = false;
+            // 
+            // btnFind
+            // 
+            this.btnFind.BackColor = System.Drawing.Color.Transparent;
+            this.btnFind.Image = ((System.Drawing.Image)(resources.GetObject("btnFind.Image")));
+            this.btnFind.ImageRotate = 0F;
+            this.btnFind.Location = new System.Drawing.Point(535, 57);
+            this.btnFind.Name = "btnFind";
+            this.btnFind.ShadowDecoration.Mode = Guna.UI2.WinForms.Enums.ShadowMode.Circle;
+            this.btnFind.Size = new System.Drawing.Size(53, 37);
+            this.btnFind.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.btnFind.TabIndex = 4;
+            this.btnFind.TabStop = false;
+            // 
+            // txtFilterValue
+            // 
+            this.txtFilterValue.AutoRoundedCorners = true;
+            this.txtFilterValue.BackColor = System.Drawing.Color.Transparent;
+            this.txtFilterValue.BorderRadius = 17;
+            this.txtFilterValue.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtFilterValue.DefaultText = "";
+            this.txtFilterValue.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txtFilterValue.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txtFilterValue.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtFilterValue.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtFilterValue.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtFilterValue.Font = new System.Drawing.Font("Andalus", 10.2F, System.Drawing.FontStyle.Bold);
+            this.txtFilterValue.ForeColor = System.Drawing.Color.Black;
+            this.txtFilterValue.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtFilterValue.Location = new System.Drawing.Point(320, 58);
+            this.txtFilterValue.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.txtFilterValue.Name = "txtFilterValue";
+            this.txtFilterValue.PlaceholderText = "";
+            this.txtFilterValue.SelectedText = "";
+            this.txtFilterValue.Size = new System.Drawing.Size(195, 36);
+            this.txtFilterValue.TabIndex = 2;
+            // 
+            // cmbFilterBy
+            // 
+            this.cmbFilterBy.AutoRoundedCorners = true;
+            this.cmbFilterBy.BackColor = System.Drawing.Color.Transparent;
+            this.cmbFilterBy.BorderRadius = 17;
+            this.cmbFilterBy.DrawMode = System.Windows.Forms.DrawMode.OwnerDrawFixed;
+            this.cmbFilterBy.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.cmbFilterBy.FillColor = System.Drawing.Color.Gainsboro;
+            this.cmbFilterBy.FocusedColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.cmbFilterBy.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.cmbFilterBy.Font = new System.Drawing.Font("Andalus", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cmbFilterBy.ForeColor = System.Drawing.Color.Black;
+            this.cmbFilterBy.ItemHeight = 30;
+            this.cmbFilterBy.Items.AddRange(new object[] {
+            "Id",
+            "National Number"});
+            this.cmbFilterBy.Location = new System.Drawing.Point(101, 58);
+            this.cmbFilterBy.Name = "cmbFilterBy";
+            this.cmbFilterBy.Size = new System.Drawing.Size(195, 36);
+            this.cmbFilterBy.TabIndex = 1;
+            // 
+            // guna2HtmlLabel1
+            // 
+            this.guna2HtmlLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.guna2HtmlLabel1.Location = new System.Drawing.Point(3, 65);
+            this.guna2HtmlLabel1.Name = "guna2HtmlLabel1";
+            this.guna2HtmlLabel1.Size = new System.Drawing.Size(54, 18);
+            this.guna2HtmlLabel1.TabIndex = 0;
+            this.guna2HtmlLabel1.Text = "Filter By:";
+            // 
+            // uc_PersonInfoCard1
+            // 
+            this.uc_PersonInfoCard1.Location = new System.Drawing.Point(3, 134);
+            this.uc_PersonInfoCard1.Name = "uc_PersonInfoCard1";
+            this.uc_PersonInfoCard1.Size = new System.Drawing.Size(728, 389);
+            this.uc_PersonInfoCard1.TabIndex = 2;
+            // 
+            // uc_PersonInfoWithFilter
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.uc_PersonInfoCard1);
+            this.Controls.Add(this.gbFilter);
+            this.Name = "uc_PersonInfoWithFilter";
+            this.Size = new System.Drawing.Size(729, 528);
+            this.gbFilter.ResumeLayout(false);
+            this.gbFilter.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.btnAdd)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.btnFind)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private Guna.UI2.WinForms.Guna2GroupBox gbFilter;
+        private Guna.UI2.WinForms.Guna2CirclePictureBox btnAdd;
+        private Guna.UI2.WinForms.Guna2CirclePictureBox btnFind;
+        private Guna.UI2.WinForms.Guna2TextBox txtFilterValue;
+        private Guna.UI2.WinForms.Guna2ComboBox cmbFilterBy;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel1;
+        private uc_PersonInfoCard uc_PersonInfoCard1;
+    }
+}
---
+++ Karate-System/People/uc_Controls/uc_PersonInfoWithFilter.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Forms;
+
+namespace KarateSystem.People.uc_Controls
+{
+    public partial class uc_PersonInfoWithFilter : UserControl
+    {
+        public uc_PersonInfoWithFilter()
+        {
+            InitializeComponent();
+        }
+
+
+    }
+}
