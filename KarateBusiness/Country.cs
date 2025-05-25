a272c8de327ecd918165df6f058cf47328c2dee3
roaa-al-homsi <roaakokalhomsi@gmail.com>
2025-05-25 12:00:23 +0300

    ADD classes country to business and access layers

---
+++ KarateBusiness/Country.cs
@@ -0,0 +1,89 @@
+﻿using KarateDataAccess;
+using System.Data;
+
+namespace KarateBusiness
+{
+    public class Country
+    {
+        private enum Mode { Add, Update }
+        private Mode _mode;
+        public int id { get; set; }
+        public string name { get; set; }
+
+        public Country()
+        {
+            this.id = 0;
+            this.name = string.Empty;
+
+            _mode = Mode.Add;
+        }
+        private Country(int id, string name)
+        {
+            this.id = id;
+            this.name = name;
+
+
+            _mode = Mode.Update;
+        }
+        private bool _Add()
+        {
+            this.id =
+                        CountryData.Add(this.name);
+            return (this.id != -1);
+        }
+
+        private bool _Update()
+        {
+            return CountryData.Update(this.id, this.name);
+        }
+        public bool Save()
+        {
+
+            switch (_mode)
+            {
+                case Mode.Add:
+                    {
+                        if (_Add())
+                        {
+                            _mode = Mode.Update;
+                            return true;
+                        }
+                        else
+                        {
+                            return false;
+                        }
+                    }
+                case Mode.Update: return _Update();
+            }
+            return false;
+        }
+        public static bool Exist(int id)
+        {
+            return CountryData.Exist(id);
+        }
+        public static bool Delete(int id)
+        {
+            if (!Exist(id))
+            {
+                return false;
+            }
+            else { return CountryData.Delete(id); }
+        }
+        public static DataTable All()
+        {
+            return CountryData.All();
+        }
+        public static Country Find(int id)
+        {
+            string name = string.Empty;
+
+            if (CountryData.Get(id, ref name))
+            {
+                return new Country(id, name);
+            }
+            return null;
+        }
+    }
+
+
+}
---
+++ KarateDataAccess/CountryData.cs
@@ -0,0 +1,112 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+
+namespace KarateDataAccess
+{
+    public static class CountryData
+    {
+        public static int Add(string name)
+        {
+            int newId = 0;
+            string query = "insert into Countries (name) values (@name) SELECT SCOPE_IDENTITY(); ";
+            using (SqlConnection connection = new SqlConnection(SettingData.ConnectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+
+                    command.Parameters.AddWithValue("@name", name);
+
+                    try
+                    {
+                        connection.Open();
+                        object result = command.ExecuteScalar();
+                        if (result != null && int.TryParse(result.ToString(), out int insertedID))
+                        {
+                            newId = insertedID;
+                        }
+
+                    }
+                    catch (Exception ex) { GenericData.LogException(ex.Message, EventLogEntryType.Error); }
+                }
+            }
+
+            return newId;
+        }
+        public static bool Update(int id, string name)
+        {
+            int RowsAffected = 0;
+            string query = "update Countries set id = @id,name = @name  WHERE id=@id;";
+            using (SqlConnection connection = new SqlConnection(SettingData.ConnectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@id", id);
+                    command.Parameters.AddWithValue("@name", name);
+
+                    try
+                    {
+                        connection.Open();
+                        RowsAffected = command.ExecuteNonQuery();
+                    }
+                    catch (Exception ex) { GenericData.LogException(ex.Message, EventLogEntryType.Error); }
+                }
+            }
+
+            return RowsAffected > 0;
+        }
+        public static bool Get(int id, ref string name)
+        {
+            bool IsFound = false;
+            string query = "select * from Countries  WHERE id=@id;";
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(SettingData.ConnectionString))
+                {
+                    connection.Open();
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@id", id);
+
+
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                IsFound = true;
+                                id = (int)reader["id"];
+                                name = (string)reader["name"];
+
+
+                            }
+                            else
+                            {
+                                IsFound = false;
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex) { GenericData.LogException(ex.Message, EventLogEntryType.Error); }
+            return IsFound;
+        }
+
+        static public DataTable All()
+        {
+            return GenericData.All("select * from Countries");
+        }
+        static public bool Delete(int id)
+        {
+            return GenericData.Delete("delete Countries where id = @id", "@id", id);
+        }
+        static public bool Exist(int id)
+        {
+            return GenericData.Exist("select Found=1 from Countries where id= @id", "@id", id);
+        }
+
+    }
+
+}
+
