586da8265c0386d12ec15e4510861b7b2127b35b
roaa-al-homsi <roaakokalhomsi@gmail.com>
2025-06-02 01:17:10 +0300

    ADD user classes to business and access layers

---
+++ KarateBusiness/User.cs
@@ -0,0 +1,98 @@
+﻿using KarateDataAccess;
+using System.Data;
+
+namespace KarateBusiness
+{
+    public class User
+    {
+        private enum Mode { Add, Update }
+        private Mode _mode;
+        public int id { get; set; }
+        public int personId { get; set; }
+        public string username { get; set; }
+        public string password { get; set; }
+        public bool isActive { get; set; }
+        public Person personInfo { get; private set; }
+
+        public User()
+        {
+            this.id = 0;
+            this.personId = -1;
+            this.username = string.Empty;
+            this.password = string.Empty;
+            this.isActive = false;
+            this.personInfo = new Person();
+            _mode = Mode.Add;
+        }
+        private User(int id, int personId, string username, string password, bool isActive)
+        {
+            this.id = id;
+            this.personId = personId;
+            this.username = username;
+            this.password = password;
+            this.isActive = isActive;
+            this.personInfo = Person.Find(personId);
+
+            _mode = Mode.Update;
+        }
+        private bool _Add()
+        {
+            this.id = UserData.Add(this.personId, this.username, this.password, this.isActive);
+            return (this.id != -1);
+        }
+
+        private bool _Update()
+        {
+            return UserData.Update(this.id, this.personId, this.username, this.password, this.isActive);
+        }
+        public bool Save()
+        {
+
+            switch (_mode)
+            {
+                case Mode.Add:
+                    {
+                        if (_Add())
+                        {
+                            _mode = Mode.Update;
+                            return true;
+                        }
+                        return false;
+                    }
+                case Mode.Update: return _Update();
+            }
+            return false;
+        }
+        public static bool Exist(int id)
+        {
+            return UserData.Exist(id);
+        }
+        public static bool Delete(int id)
+        {
+            if (!Exist(id))
+            {
+                return false;
+            }
+            else { return UserData.Delete(id); }
+        }
+        public static DataTable All()
+        {
+            return UserData.All();
+        }
+        public static User Find(int id)
+        {
+            int personId = -1;
+            string username = string.Empty;
+            string password = string.Empty;
+            bool isActive = false;
+
+            if (UserData.Get(id, ref personId, ref username, ref password, ref isActive))
+            {
+                return new User(id, personId, username, password, isActive);
+            }
+            return null;
+        }
+    }
+
+
+}
---
+++ KarateDataAccess/UserData.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Data;
+using System.Data.SqlClient;
+using System.Diagnostics;
+
+namespace KarateDataAccess
+{
+    public static class UserData
+    {
+        public static int Add(int personId, string username, string password, bool isActive)
+        {
+            int newId = 0;
+            string query = "insert into Users (personId,username,password,isActive) values (@personId,@username,@password,@isActive) SELECT SCOPE_IDENTITY(); ";
+            using (SqlConnection connection = new SqlConnection(SettingData.ConnectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+
+                    command.Parameters.AddWithValue("@personId", personId);
+                    command.Parameters.AddWithValue("@username", username);
+                    command.Parameters.AddWithValue("@password", password);
+                    command.Parameters.AddWithValue("@isActive", isActive);
+
+                    try
+                    {
+                        connection.Open();
+                        object result = command.ExecuteScalar();
+                        if (result != null && int.TryParse(result.ToString(), out int insertedID))
+                        {
+                            newId = insertedID;
+                        }
+
+                    }
+                    catch (Exception ex) { GenericData.LogException(ex.Message, EventLogEntryType.Error); }
+                }
+            }
+
+            return newId;
+        }
+        public static bool Update(int id, int personId, string username, string password, bool isActive)
+        {
+            int RowsAffected = 0;
+            string query = "update Users set personId = @personId,username = @username,password = @password,isActive = @isActive  WHERE id=@id;";
+            using (SqlConnection connection = new SqlConnection(SettingData.ConnectionString))
+            {
+                using (SqlCommand command = new SqlCommand(query, connection))
+                {
+                    command.Parameters.AddWithValue("@id", id);
+                    command.Parameters.AddWithValue("@personId", personId);
+                    command.Parameters.AddWithValue("@username", username);
+                    command.Parameters.AddWithValue("@password", password);
+                    command.Parameters.AddWithValue("@isActive", isActive);
+
+                    try
+                    {
+                        connection.Open();
+                        RowsAffected = command.ExecuteNonQuery();
+                    }
+                    catch (Exception ex) { GenericData.LogException(ex.Message, EventLogEntryType.Error); }
+                }
+            }
+
+            return RowsAffected > 0;
+        }
+        public static bool Get(int id, ref int personId, ref string username, ref string password, ref bool isActive)
+        {
+            bool IsFound = false;
+            string query = "select * from Users  WHERE id=@id;";
+
+            try
+            {
+                using (SqlConnection connection = new SqlConnection(SettingData.ConnectionString))
+                {
+                    connection.Open();
+                    using (SqlCommand command = new SqlCommand(query, connection))
+                    {
+                        command.Parameters.AddWithValue("@id", id);
+
+
+                        using (SqlDataReader reader = command.ExecuteReader())
+                        {
+                            if (reader.Read())
+                            {
+                                IsFound = true;
+                                id = (int)reader["id"];
+                                personId = (int)reader["personId"];
+                                username = (string)reader["username"];
+                                password = (string)reader["password"];
+                                isActive = (bool)reader["isActive"];
+
+
+                            }
+                            else
+                            {
+                                IsFound = false;
+                            }
+                        }
+                    }
+                }
+            }
+            catch (Exception ex) { GenericData.LogException(ex.Message, EventLogEntryType.Error); }
+            return IsFound;
+        }
+
+        static public DataTable All()
+        {
+            return GenericData.All("select * from view_User_Info");
+        }
+        static public bool Delete(int id)
+        {
+            return GenericData.Delete("delete Users where id = @id", "@id", id);
+        }
+        static public bool Exist(int id)
+        {
+            return GenericData.Exist("select Found=1 from Users where id= @id", "@id", id);
+        }
+
+    }
+
+}
+
