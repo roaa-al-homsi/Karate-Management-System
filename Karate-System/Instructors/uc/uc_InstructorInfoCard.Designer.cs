cf52ace783bbd265b7b45f3932b73717853218c4
roaa-al-homsi <roaakokalhomsi@gmail.com>
2025-06-15 22:34:35 +0300

    ADD uc_InstructorInfoCard

---
+++ Karate-System/Instructors/uc/uc_InstructorInfoCard.Designer.cs
@@ -0,0 +1,212 @@
+﻿namespace KarateSystem.Instructors.uc
+{
+    partial class uc_InstructorInfoCard
+    {
+        /// <summary> 
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary> 
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Component Designer generated code
+
+        /// <summary> 
+        /// Required method for Designer support - do not modify 
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(uc_InstructorInfoCard));
+            this.uc_PersonInfoCard1 = new KarateSystem.People.uc_Controls.uc_PersonInfoCard();
+            this.panelLoginInfo = new Guna.UI2.WinForms.Guna2CustomGradientPanel();
+            this.linkLabEditInstructor = new System.Windows.Forms.LinkLabel();
+            this.guna2PictureBox2 = new Guna.UI2.WinForms.Guna2PictureBox();
+            this.guna2PictureBox1 = new Guna.UI2.WinForms.Guna2PictureBox();
+            this.txtQualifications = new Guna.UI2.WinForms.Guna2TextBox();
+            this.txtInstructorId = new Guna.UI2.WinForms.Guna2TextBox();
+            this.guna2HtmlLabel2 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.guna2HtmlLabel1 = new Guna.UI2.WinForms.Guna2HtmlLabel();
+            this.panelLoginInfo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // uc_PersonInfoCard1
+            // 
+            this.uc_PersonInfoCard1.Location = new System.Drawing.Point(0, 0);
+            this.uc_PersonInfoCard1.Name = "uc_PersonInfoCard1";
+            this.uc_PersonInfoCard1.Size = new System.Drawing.Size(728, 394);
+            this.uc_PersonInfoCard1.TabIndex = 0;
+            // 
+            // panelLoginInfo
+            // 
+            this.panelLoginInfo.BorderColor = System.Drawing.Color.Black;
+            this.panelLoginInfo.BorderThickness = 1;
+            this.panelLoginInfo.Controls.Add(this.linkLabEditInstructor);
+            this.panelLoginInfo.Controls.Add(this.guna2PictureBox2);
+            this.panelLoginInfo.Controls.Add(this.guna2PictureBox1);
+            this.panelLoginInfo.Controls.Add(this.txtQualifications);
+            this.panelLoginInfo.Controls.Add(this.txtInstructorId);
+            this.panelLoginInfo.Controls.Add(this.guna2HtmlLabel2);
+            this.panelLoginInfo.Controls.Add(this.guna2HtmlLabel1);
+            this.panelLoginInfo.Location = new System.Drawing.Point(3, 400);
+            this.panelLoginInfo.Name = "panelLoginInfo";
+            this.panelLoginInfo.Size = new System.Drawing.Size(718, 109);
+            this.panelLoginInfo.TabIndex = 5;
+            // 
+            // linkLabEditInstructor
+            // 
+            this.linkLabEditInstructor.AutoSize = true;
+            this.linkLabEditInstructor.BackColor = System.Drawing.Color.Transparent;
+            this.linkLabEditInstructor.DisabledLinkColor = System.Drawing.Color.Transparent;
+            this.linkLabEditInstructor.Font = new System.Drawing.Font("Aldhabi", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.linkLabEditInstructor.Location = new System.Drawing.Point(555, 15);
+            this.linkLabEditInstructor.Name = "linkLabEditInstructor";
+            this.linkLabEditInstructor.Size = new System.Drawing.Size(152, 35);
+            this.linkLabEditInstructor.TabIndex = 40;
+            this.linkLabEditInstructor.TabStop = true;
+            this.linkLabEditInstructor.Text = "Edit Instructor Info";
+            // 
+            // guna2PictureBox2
+            // 
+            this.guna2PictureBox2.BackColor = System.Drawing.Color.Transparent;
+            this.guna2PictureBox2.Image = ((System.Drawing.Image)(resources.GetObject("guna2PictureBox2.Image")));
+            this.guna2PictureBox2.ImageRotate = 0F;
+            this.guna2PictureBox2.Location = new System.Drawing.Point(26, 57);
+            this.guna2PictureBox2.Name = "guna2PictureBox2";
+            this.guna2PictureBox2.Size = new System.Drawing.Size(32, 36);
+            this.guna2PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.guna2PictureBox2.TabIndex = 11;
+            this.guna2PictureBox2.TabStop = false;
+            this.guna2PictureBox2.UseTransparentBackground = true;
+            // 
+            // guna2PictureBox1
+            // 
+            this.guna2PictureBox1.BackColor = System.Drawing.Color.Transparent;
+            this.guna2PictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("guna2PictureBox1.Image")));
+            this.guna2PictureBox1.ImageRotate = 0F;
+            this.guna2PictureBox1.Location = new System.Drawing.Point(26, 9);
+            this.guna2PictureBox1.Name = "guna2PictureBox1";
+            this.guna2PictureBox1.Size = new System.Drawing.Size(32, 36);
+            this.guna2PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.guna2PictureBox1.TabIndex = 5;
+            this.guna2PictureBox1.TabStop = false;
+            this.guna2PictureBox1.UseTransparentBackground = true;
+            // 
+            // txtQualifications
+            // 
+            this.txtQualifications.AutoRoundedCorners = true;
+            this.txtQualifications.BackColor = System.Drawing.Color.Transparent;
+            this.txtQualifications.BorderColor = System.Drawing.Color.Transparent;
+            this.txtQualifications.BorderRadius = 16;
+            this.txtQualifications.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtQualifications.DefaultText = "";
+            this.txtQualifications.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txtQualifications.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txtQualifications.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtQualifications.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtQualifications.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtQualifications.Font = new System.Drawing.Font("Andalus", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtQualifications.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtQualifications.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtQualifications.Location = new System.Drawing.Point(354, 52);
+            this.txtQualifications.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtQualifications.Name = "txtQualifications";
+            this.txtQualifications.PlaceholderForeColor = System.Drawing.Color.Transparent;
+            this.txtQualifications.PlaceholderText = "";
+            this.txtQualifications.ReadOnly = true;
+            this.txtQualifications.SelectedText = "";
+            this.txtQualifications.Size = new System.Drawing.Size(177, 34);
+            this.txtQualifications.TabIndex = 7;
+            this.txtQualifications.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtQualifications_KeyPress);
+            // 
+            // txtInstructorId
+            // 
+            this.txtInstructorId.AutoRoundedCorners = true;
+            this.txtInstructorId.BackColor = System.Drawing.Color.Transparent;
+            this.txtInstructorId.BorderColor = System.Drawing.Color.Transparent;
+            this.txtInstructorId.BorderRadius = 16;
+            this.txtInstructorId.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.txtInstructorId.DefaultText = "";
+            this.txtInstructorId.DisabledState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(208)))), ((int)(((byte)(208)))), ((int)(((byte)(208)))));
+            this.txtInstructorId.DisabledState.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(226)))), ((int)(((byte)(226)))), ((int)(((byte)(226)))));
+            this.txtInstructorId.DisabledState.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtInstructorId.DisabledState.PlaceholderForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(138)))), ((int)(((byte)(138)))), ((int)(((byte)(138)))));
+            this.txtInstructorId.FocusedState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtInstructorId.Font = new System.Drawing.Font("Andalus", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtInstructorId.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.txtInstructorId.HoverState.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(94)))), ((int)(((byte)(148)))), ((int)(((byte)(255)))));
+            this.txtInstructorId.Location = new System.Drawing.Point(354, 11);
+            this.txtInstructorId.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.txtInstructorId.Name = "txtInstructorId";
+            this.txtInstructorId.PlaceholderForeColor = System.Drawing.Color.Transparent;
+            this.txtInstructorId.PlaceholderText = "";
+            this.txtInstructorId.ReadOnly = true;
+            this.txtInstructorId.SelectedText = "";
+            this.txtInstructorId.Size = new System.Drawing.Size(177, 34);
+            this.txtInstructorId.TabIndex = 6;
+            this.txtInstructorId.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.txtInstructorId_KeyPress);
+            // 
+            // guna2HtmlLabel2
+            // 
+            this.guna2HtmlLabel2.AutoSize = false;
+            this.guna2HtmlLabel2.BackColor = System.Drawing.Color.Transparent;
+            this.guna2HtmlLabel2.Font = new System.Drawing.Font("Andalus", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2HtmlLabel2.Location = new System.Drawing.Point(64, 57);
+            this.guna2HtmlLabel2.Name = "guna2HtmlLabel2";
+            this.guna2HtmlLabel2.Size = new System.Drawing.Size(170, 30);
+            this.guna2HtmlLabel2.TabIndex = 1;
+            this.guna2HtmlLabel2.Text = "Qualifications:";
+            // 
+            // guna2HtmlLabel1
+            // 
+            this.guna2HtmlLabel1.AutoSize = false;
+            this.guna2HtmlLabel1.BackColor = System.Drawing.Color.Transparent;
+            this.guna2HtmlLabel1.Font = new System.Drawing.Font("Andalus", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2HtmlLabel1.Location = new System.Drawing.Point(64, 15);
+            this.guna2HtmlLabel1.Name = "guna2HtmlLabel1";
+            this.guna2HtmlLabel1.Size = new System.Drawing.Size(170, 30);
+            this.guna2HtmlLabel1.TabIndex = 0;
+            this.guna2HtmlLabel1.Text = "Instructor Id:";
+            // 
+            // uc_InstructorInfoCard
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.panelLoginInfo);
+            this.Controls.Add(this.uc_PersonInfoCard1);
+            this.Name = "uc_InstructorInfoCard";
+            this.Size = new System.Drawing.Size(729, 517);
+            this.panelLoginInfo.ResumeLayout(false);
+            this.panelLoginInfo.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.guna2PictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private People.uc_Controls.uc_PersonInfoCard uc_PersonInfoCard1;
+        private Guna.UI2.WinForms.Guna2CustomGradientPanel panelLoginInfo;
+        private System.Windows.Forms.LinkLabel linkLabEditInstructor;
+        private Guna.UI2.WinForms.Guna2PictureBox guna2PictureBox2;
+        private Guna.UI2.WinForms.Guna2PictureBox guna2PictureBox1;
+        private Guna.UI2.WinForms.Guna2TextBox txtQualifications;
+        private Guna.UI2.WinForms.Guna2TextBox txtInstructorId;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel2;
+        private Guna.UI2.WinForms.Guna2HtmlLabel guna2HtmlLabel1;
+    }
+}
---
+++ Karate-System/Instructors/uc/uc_InstructorInfoCard.cs
@@ -0,0 +1,64 @@
+﻿using KarateBusiness;
+using System.Windows.Forms;
+
+namespace KarateSystem.Instructors.uc
+{
+    public partial class uc_InstructorInfoCard : UserControl
+    {
+        private int _instructorId;
+
+        public int InstructorId
+        {
+            get
+            {
+                return _instructorId;
+            }
+            set
+            {
+                _instructorId = value;
+            }
+        }
+
+        private Instructor _instructor;
+        public Instructor InstructorSelected
+        {
+            get { return _instructor; }
+        }
+        public uc_InstructorInfoCard()
+        {
+            InitializeComponent();
+        }
+        private void _LoadDataToForm()
+        {
+            uc_PersonInfoCard1.LoadPersonInfo(_instructor.personId);
+            txtInstructorId.Text = _instructorId.ToString();
+            txtQualifications.Text = _instructor.qualifications;
+        }
+        public void LoadInstructorInfo(int instructorId)
+        {
+            _instructor = Instructor.Find(instructorId);
+            if (_instructor == null)
+            {
+                MessageBox.Show($"There isn't instructor with this Id:{instructorId}", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+            _instructorId = instructorId;
+            _LoadDataToForm();
+        }
+
+        private void txtInstructorId_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsDigit(e.KeyChar) && !char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+        private void txtQualifications_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if (!char.IsLetter(e.KeyChar) && !char.IsControl(e.KeyChar))
+            {
+                e.Handled = true;
+            }
+        }
+    }
+}
