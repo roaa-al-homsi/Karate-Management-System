f53a35252df3419199b9c9e167f0bbb879186781
roaa-al-homsi <roaakokalhomsi@gmail.com>
2025-06-18 20:01:38 +0300

    ADD frmShowMemberInstructorDetails

---
+++ Karate-System/Members_Instructors/frmShowMemberInstructorDetails.cs
@@ -0,0 +1,22 @@
+﻿using System.Windows.Forms;
+
+namespace KarateSystem.Members_Instructors
+{
+    public partial class frmShowMemberInstructorDetails : Form
+    {
+        private int _memberInstructorId = -1;
+
+        public frmShowMemberInstructorDetails(int memberInstructorId)
+        {
+            InitializeComponent();
+            _memberInstructorId = memberInstructorId;
+
+        }
+        private void
+        private void linkLabEditMemberInstructor_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            frmAddUpdateMembersInstructors frmAddUpdateMembersInstructors = new frmAddUpdateMembersInstructors();
+            frmAddUpdateMembersInstructors.ShowDialog();
+        }
+    }
+}
